91cf12f3e8bfc9b1a55caebb466e557154ac62f0
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2022-11-25 22:09:08 +0700

    refactor: rename get photo route

--- competition/routes/photo.go
+++ competition/routes/photo.go
@@ -8,7 +8,7 @@ import (
 )
 
 func PhotoRoute(route *gin.Engine) {
-	route.GET("/get-photo-data", middlewares.AuthMiddleware(), controllers.GetPhotoHandler())
+	route.GET("/get-photo", middlewares.AuthMiddleware(), controllers.GetPhotoHandler())
 	route.POST("/add-photo", middlewares.AuthMiddleware(), controllers.AddPhotoHandler())
 	route.DELETE("/delete-photo", middlewares.AuthMiddleware(), controllers.DeletePhotoHandler())
 }
