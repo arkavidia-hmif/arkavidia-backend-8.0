7eec6ebc7fff807968492c7666cd93859ce84d34
reyshazni013 <18220013@mahasiswa.itb.ac.id>
2023-01-05 18:16:07 +0700

    add unique constraints

--- competition/models/photo.go
+++ competition/models/photo.go
@@ -13,7 +13,7 @@ import (
 
 type Photo struct {
 	gorm.Model
-	FileName      uuid.UUID         `gorm:"type:uuid"`
+	FileName      uuid.UUID         `gorm:"type:uuid;unique"`
 	FileExtension string            `gorm:"not null"`
 	ParticipantID uint              `gorm:"not null"`
 	AdminID       uint              `gorm:"default:null"`
